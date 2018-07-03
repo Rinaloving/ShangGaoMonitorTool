428fd6db04532f62dd56922ad60c7d54fbdd4a0d
Rina <cfl1989@live.com>
2018-07-04 07:43:20 +0800

    实时更新

---
+++ ShangGaoMonitorTool/ShangGaoMonitorTool/ShangGaoMonitorTool/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace ShangGaoMonitorTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.DataPoint dataPoint1 = new System.Windows.Forms.DataVisualization.Charting.DataPoint(0D, 0D);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 450);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.chart1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel2.Location = new System.Drawing.Point(205, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(514, 450);
+            this.panel2.TabIndex = 1;
+            // 
+            // chart1
+            // 
+            chartArea1.AxisX.Interval = 1D;
+            chartArea1.AxisX.IntervalOffset = 1D;
+            chartArea1.AxisX.IntervalOffsetType = System.Windows.Forms.DataVisualization.Charting.DateTimeIntervalType.Days;
+            chartArea1.AxisX.IntervalType = System.Windows.Forms.DataVisualization.Charting.DateTimeIntervalType.Days;
+            chartArea1.CursorX.IntervalOffset = 1D;
+            chartArea1.CursorX.IntervalOffsetType = System.Windows.Forms.DataVisualization.Charting.DateTimeIntervalType.Days;
+            chartArea1.CursorX.IntervalType = System.Windows.Forms.DataVisualization.Charting.DateTimeIntervalType.Days;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Docking = System.Windows.Forms.DataVisualization.Charting.Docking.Top;
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(3, 0);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            series1.Points.Add(dataPoint1);
+            series1.XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.DateTime;
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(511, 452);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            this.chart1.Click += new System.EventHandler(this.chart1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(719, 450);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    }
+}
+
---
+++ ShangGaoMonitorTool/ShangGaoMonitorTool/ShangGaoMonitorTool/Form1.cs
@@ -0,0 +1,273 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace ShangGaoMonitorTool
+{
+    public partial class Form1 : Form
+    {
+        //private Queue<double> dataQueue = new Queue<double>(7);
+        private Queue<double> dataQueue = new Queue<double>(7);
+
+        private Queue<string> dtq = new Queue<string>(7);
+
+        private static int filenums = 0;
+
+        private int curValue = 0;
+
+        private int num = 1;//每次删除增加几个点
+
+        private static int j = -1; //日期初始值
+
+        static System.Timers.Timer timer;
+        //string[] dt = new string[7];
+        DateTime date = DateTime.Now.Date;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
+        }
+
+
+        /// <summary>
+        /// 初始化事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnInit_Click(object sender, EventArgs e)
+        {
+            InitChart();
+        }
+        ///// <summary>
+        ///// 开始事件
+        ///// </summary>
+        ///// <param name="sender"></param>
+        ///// <param name="e"></param>
+        //private void btnStart_Click(object sender, EventArgs e)
+        //{
+        //    this.timer1.Start();
+        //}
+        ///// <summary>
+        ///// 停止事件
+        ///// </summary>
+        ///// <param name="sender"></param>
+        ///// <param name="e"></param>
+        //private void btnStop_Click(object sender, EventArgs e)
+        //{
+        //    this.timer1.Stop();
+        //}
+
+        /// <summary>
+        /// 初始化图表
+        /// </summary>
+        private void InitChart()
+        {
+            
+
+            //dt[0] = date.AddDays(-1).ToString("M");
+            //dt[1] = date.AddDays(0).ToString("M");
+            //dt[2] = date.AddDays(1).ToString("M");
+            //dt[3] = date.AddDays(2).ToString("M");
+            //dt[4] = date.AddDays(3).ToString("M");
+            //dt[5] = date.AddDays(4).ToString("M");
+            //dt[6] = date.AddDays(5).ToString("M");
+            //定义图表区域
+            this.chart1.ChartAreas.Clear();
+            ChartArea chartArea1 = new ChartArea("C1");
+            this.chart1.ChartAreas.Add(chartArea1);
+            //定义存储和显示点的容器
+            this.chart1.Series.Clear();
+            Series series1 = new Series("数量（件）");
+            series1.ChartArea = "C1";
+            this.chart1.Series.Add(series1);
+            //设置图表显示样式
+            this.chart1.ChartAreas[0].AxisY.Minimum = 0;
+            this.chart1.ChartAreas[0].AxisY.Maximum = 100;
+            //this.chart1.ChartAreas[0].AxisX.IntervalType = DateTimeIntervalType.Days;
+            this.chart1.ChartAreas[0].AxisX.Minimum = 1; //坐标最小值 
+            this.chart1.ChartAreas[0].AxisX.Maximum = 7;//坐标最大值
+            //this.chart1.ChartAreas[0].AxisX.Interval = 1;//坐标大刻度间隔
+            //this.chart1.ChartAreas[0].AxisX.MinorTickMark.Interval = 1;//小刻度间隔，就像厘米下的毫米一样
+            this.chart1.ChartAreas[0].AxisX.MajorGrid.LineColor = System.Drawing.Color.Silver;
+            this.chart1.ChartAreas[0].AxisY.MajorGrid.LineColor = System.Drawing.Color.Silver;
+            //this.chart1.ChartAreas[0].AxisX.LabelStyle.Format = DateTime.Now.GetDateTimeFormats('f')[0].ToString();  //GetDateTimeFormats('M')[0].ToString();//
+            // this.chart1.ChartAreas[0].AxisX.LabelStyle.Format = DateTime.Now.GetDateTimeFormats('M')[0].ToString();
+            //this.chart1.AlignDataPointsByAxisLabel.ActiveControl= DateTime.Now.GetDateTimeFormats('M')[0].ToString();
+           
+            //for (int i = 0; i < dt.Length; i++)
+            //{
+            //    this.chart1.ChartAreas[0].AxisX.LabelStyle.Format = dt[i];  // XValueMember 
+            //                                                                //this.chart1.ChartAreas[0].AxisX. = dt[i];  // XValueMember 
+
+            //}
+
+
+            //11月5日; // 11月5日 DateTime.Now.AddDays(1).ToShortDateString();
+            // this.chart1.ChartAreas[0].AxisX.LabelStyle.Angle = "一个";
+            // DateTime.Now.GetDateTimeFormats('M').ToString()   DateTime.Now.AddDays(1).ToShortDateString()
+
+            //设置标题
+            this.chart1.Titles.Clear();
+            this.chart1.Titles.Add("");
+            this.chart1.Titles[0].Text = "";
+            this.chart1.Titles[0].ForeColor = Color.RoyalBlue;
+            this.chart1.Titles[0].Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            //设置图表显示样式
+            this.chart1.Series[0].Color = Color.Red;
+          
+            this.chart1.Titles[0].Text = string.Format("");
+            this.chart1.Series[0].ChartType = SeriesChartType.Line;
+           
+            this.chart1.Series[0].Points.Clear();
+        }
+
+        //更新队列中的值
+        private  void UpdateQueueValue()
+        {
+
+            if (dataQueue.Count > 7)
+            {
+                //先出列
+                for (int i = 0; i < num; i++)
+                {
+                    dataQueue.Dequeue();
+                    dtq.Dequeue();
+                }
+            }
+           
+            //Random r = new Random();
+            for (int i = 0; i < num; i++)
+            {
+                //对curValue只取[0,360]之间的值
+               // curValue = curValue % 360;
+                //对得到的正玄值，放大50倍，并上移50
+               // dataQueue.Enqueue((filenums * Math.Sin(curValue * Math.PI / 180)) + filenums);
+               // curValue = curValue + 10;
+                dataQueue.Enqueue(filenums); // dataQueue.Enqueue(r.Next(0, 100));
+                Thread.Sleep(2000);
+                dtq.Enqueue(date.AddDays(j++).ToString("M"));
+            }
+          
+        }
+
+
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           // UpdateQueueValue();
+            InitChart();
+            UpdateQueueValue();
+            this.chart1.Series[0].Points.Clear();
+            for (int i = 0; i < dataQueue.Count; i++)
+            {
+                //this.chart1.Series[0].Points.AddXY((i + 1), dataQueue.ElementAt(i)); //dt
+                this.chart1.Series[0].Points.AddXY(dtq.ElementAt(i), dataQueue.ElementAt(i));
+
+            }
+
+            schedule_Timer();
+            
+        }
+
+        public void schedule_Timer()
+        {
+            Console.WriteLine("### Timer Started ###");
+
+            DateTime nowTime = DateTime.Now;
+            DateTime scheduledTime = new DateTime(nowTime.Year, nowTime.Month, nowTime.Day, 17, 21, 0, 0); //指定预设时间  HH,MM,SS [8am and 42 minutes]
+            if (nowTime > scheduledTime)
+            {
+                scheduledTime = scheduledTime.AddDays(1);
+
+            }
+
+            //double tickTime = (double)(scheduledTime - DateTime.Now).TotalMilliseconds;
+            timer = new System.Timers.Timer(60000);
+            timer.Enabled = true;
+            timer.Elapsed += new System.Timers.ElapsedEventHandler(timer_Elapsed);
+            // www.lupotian3.vip
+            // Console.WriteLine("计划时间：" + scheduledTime + "执行时间："+DateTime.UtcNow);
+            timer.Start();
+        }
+
+        public void timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            Console.WriteLine("### Timer Stopped ### \n");
+            timer.Stop();
+            //Console.WriteLine("### Scheduled Task Started ### \n\n");
+            //Console.WriteLine("Hello World!!! - Performing scheduled task\n");
+            //Console.WriteLine("### Task Finished ### \n\n");
+
+            string fileBacksPath = @"D:\sgxml\Up\Backs";
+            string fileUpPath = @"D:\sgxml\Up\";
+            removeUnUploadFile(fileBacksPath, fileUpPath);
+            //InitChart();
+            UpdateQueueValue();
+            this.chart1.Series[0].Points.Clear();
+            for (int i = 0; i < dataQueue.Count; i++)
+            {
+                //this.chart1.Series[0].Points.AddXY((i + 1), dataQueue.ElementAt(i));
+                this.chart1.Series[0].Points.AddXY(dtq.ElementAt(i), dataQueue.ElementAt(i));
+
+            }
+            schedule_Timer();
+        }
+
+        public  void removeUnUploadFile(string fileBacksPath, string fileUpPath)
+        {
+            //Console.WriteLine(DateTime.Today); //2018/7/3/0000
+           
+
+            FileInfo[] fileInfos = new DirectoryInfo(fileBacksPath).GetFiles("*.xml");
+            filenums = fileInfos.Count();
+            //如果这个文件夹下存在文件
+            if (fileInfos.Count() > 0)
+            {
+                foreach (var item in fileInfos)
+                {
+                    //Console.WriteLine("文件全路径：" + item.FullName);
+                    //Console.WriteLine("文件：" + item.Name);
+                    //Console.WriteLine("文件修改时间：" + item.LastWriteTime);
+                    //如果文件的创建时间大于当天子时时间，说明是今天创建的，那么就移动到Up文件夹下，由上报工具重新上传
+                    if (DateTime.Today > item.LastWriteTime)
+                    {
+                        //把这些文件移动到Up文件夹下
+                        try
+                        {
+                            File.Move(item.FullName, fileUpPath + item.Name);
+                        }
+                        catch (Exception ex)
+                        {
+
+                            throw;
+                        }
+
+
+                    }
+                }
+            }
+        }
+
+
+
+
+
+
+    }
+}
